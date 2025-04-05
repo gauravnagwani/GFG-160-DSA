b84d21c5064a0e795874ce954ce1dc39b492bc06
Gaurav Nagwani <gauravnagwani859@gmail.com>
2025-04-05 13:17:56 +0530

    Majority Element 2

---
+++ DAY-6.java
@@ -0,0 +1,43 @@
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+        int count1 =0 , count2 = 0, maj1 = 0 , maj2 =0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]==maj1){
+                count1++;
+            }
+            else if(nums[i] == maj2){
+                count2++;
+            }
+            else if(count1==0){
+                maj1 = nums[i];
+                count1=1;
+            }
+            else if(count2==0){
+                maj2 = nums[i];
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        //verification
+        count1=0;
+        count2=0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i] == maj1){
+                count1++;
+            }
+            else if(nums[i]== maj2){
+                count2++;
+            }
+        }
+        List<Integer>result = new ArrayList<>();
+        int threshold = nums.length/3;
+        if(count1>threshold) result.add(maj1);
+        if(count2>threshold) result.add(maj2);
+    Collections.sort(result);
+    return result;
+    }
+}
